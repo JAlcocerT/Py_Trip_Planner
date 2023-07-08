515b2b041fdba02aa6ac5d2990a346b8f0e42bc9
Jesus Alcocer Tagua <56076760+JAlcocerT@users.noreply.github.com>
2023-07-08 15:53:55 +0200

    v1.3 to app file

--- app/app.py
+++ app/app.py
@@ -13,6 +13,8 @@ import dash_leaflet as dl
 
 #from jupyter_dash import JupyterDash
 
+#V1.3
+
 #app = JupyterDash(__name__, external_stylesheets=['https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'])
 app = dash.Dash(__name__, external_stylesheets=['https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'])
 
@@ -22,17 +24,24 @@ def plot_tmax_boxplot(lat, lon, start, end, variable):
     data = Daily(location, start, end)
     data = data.fetch()
 
-    data = data.reset_index()
+    df_meteo_boxplot = data.reset_index()
 
-    data['month'] = data['time'].dt.month_name()
+    df_meteo_boxplot['month'] = df_meteo_boxplot['time'].dt.month_name()
 
-    fig = px.box(data, x='month', y=variable, title='Monthly Summaries -  Boxplot')
+    fig = px.box(df_meteo_boxplot, x='month', y=variable, title='Monthly Summaries -  Boxplot')
 
     fig.update_xaxes(title_text='Month')
     fig.update_yaxes(title_text=('Max Temperature (°C)' if variable == 'tmax' else
                               'Min Temperature (°C)' if variable == 'tmin' else
                               'Wind (km/h)' if variable == 'wspd' else
                               'Rain (mm)'))
+
+    fig.update_traces(marker_color='#21B1CF', line_color='#21B1CF')
+
+
+    fig.update_layout(plot_bgcolor='#2E2E33',
+                      paper_bgcolor='#F5F5F5')
+    
     return fig
 
 def plot_weather_data(lat, lon, start, end):
@@ -44,25 +53,33 @@ def plot_weather_data(lat, lon, start, end):
     data = data.reset_index()
 
     # Rename the columns
-    data = data.rename(columns={'tmin': 'Tmin',
+    df_meteostat = data.rename(columns={'tmin': 'Tmin',
                            'tmax': 'Tmax'})
 
-    fig = px.line(data, x='time', y=['Tmin', 'Tmax'],
+    fig = px.line(df_meteostat, x='time', y=['Tmin', 'Tmax'],
                   title=f'Temperature Min/Max for the Given Location ({lat:.2f}, {lon:.2f})',
-                  labels={'time': 'Date'})
+                  labels={'time': 'Date'},
+                  color_discrete_sequence=['#66E5FF', 'orange'])
     fig.update_yaxes(title_text='Temperature °C')
 
     fig.data[0].name = 'Tmin'
     fig.data[1].name = 'Tmax'
 
+
     # Update the legend title
-    fig.update_layout(legend_title='Legend')
+    fig.update_layout(legend_title='Legend',
+                      plot_bgcolor='#2E2E33',
+                      paper_bgcolor='#F5F5F5')
 
     return fig
 
 
-app.layout = html.Div([
-    html.H1('Trip Planner', style={'textAlign': 'center', 'padding': '20px'}),
+
+
+app.layout = html.Div(
+    style={'backgroundColor': '#F5F5F5'},
+    children=[
+    html.H1('Trip Planner', style={'textAlign': 'center', 'padding': '20px','backgroundColor':'F5F5F5'}),
     dcc.DatePickerRange(
         id='date-picker',
         min_date_allowed=datetime(2000, 1, 1),
@@ -72,7 +89,7 @@ app.layout = html.Div([
         display_format='MMM DD, YYYY',
         style = {
                         'font-size': '6px','display': 'inline-block', 'border-radius' : '2px', 
-                        'border' : '1px solid #ccc', 'color': '#333', 
+                        'border' : '1px solid #ccc', 'color': '#2E2E33', 
                         'border-spacing' : '0', 'border-collapse' :'separate'
                         } 
     ),
@@ -82,7 +99,6 @@ app.layout = html.Div([
         style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"},
         center=[35, 25],
         zoom=4,
-        #click_lat_lng=True,
     ),
     dcc.Graph(id='weather-plot'),
     html.Label('Select a variable to display in the Boxplot:'),
@@ -97,32 +113,41 @@ app.layout = html.Div([
         value='tmax'
     ),
     dcc.Graph(id='tmax-boxplot'),
-    html.Div([
-        html.H4("About"),
-        html.A("My Blog - FossEngineer", href="https://fossengineer.com", target="_blank"),
-        html.Br(),
-        html.A("About this App - FossEngineer", href="https://fossengineer.com/python-trip-planner/", target="_blank"),
-        html.Br(),
-        html.A("Source Code", href="https://github.com/JAlcocerT/Py_Trip_Planner/", target="_blank"),
-    ], style={'float': 'right'})
+   
+   html.Div(
+    [
+        html.Div(
+            [
+                html.H4("About"),
+                html.A("My Blog - FossEngineer", href="https://fossengineer.com", target="_blank"),
+                html.Br(),
+                html.A("About this App - FossEngineer", href="https://fossengineer.com/python-trip-planner/", target="_blank"),
+                html.Br(),
+                html.A("Source Code", href="https://github.com/JAlcocerT/Py_Trip_Planner/", target="_blank"),
+            ],
+            style={'backgroundColor': '#F5F5F5', 'text-align': 'center'}
+        ),
+    ],
+    style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'display': 'inline-block', 'backgroundColor': '#F5F5F5'}
+
+)
+
+    
 ])
 
 @app.callback(
     Output('layer', 'children'),
     [Input('map', 'click_lat_lng')],
 )
-# def update_markers(click_lat_lng):
-#     if not click_lat_lng:
-#         click_lat_lng = [35, 25]
-#     return [dl.Marker(position=click_lat_lng, children=dl.Tooltip(f"({click_lat_lng[0]:.2f}, {click_lat_lng[1]:.2f})"))]
+
+
 
 def update_markers(click_lat_lng):
     if not click_lat_lng:
         click_lat_lng = [35, 25]
     return [
         dl.Marker(position=click_lat_lng, children=dl.Tooltip(f"({click_lat_lng[0]:.2f}, {click_lat_lng[1]:.2f})")),
-        #dl.Marker(position=[nearest_alt, nearest_lon], children=dl.Tooltip(f"({nearest_alt:.2f}, {nearest_lon:.2f})"))
-        dl.CircleMarker(center=[nearest_alt, nearest_lon], color="#188399",)
+        dl.CircleMarker(center=[nearest_lat, nearest_lon], color="#188399",)
     ]
 
 @app.callback(
@@ -130,7 +155,7 @@ def update_markers(click_lat_lng):
     [Input('map', 'click_lat_lng'), Input('date-picker', 'start_date'), Input('date-picker', 'end_date'), Input('boxplot-variable', 'value')],
 )
 def update_weather_plots(click_lat_lng, start_date, end_date, boxplot_variable):
-    global nearest_alt, nearest_lon
+    global nearest_lat, nearest_lon
     if not click_lat_lng:
         lat, lon = 35, 25
     else:
@@ -142,22 +167,17 @@ def update_weather_plots(click_lat_lng, start_date, end_date, boxplot_variable):
     stations = stations.nearby(lat, lon) 
     station = stations.fetch(1) #the closest station to the input location
 
-    nearest_alt = station['latitude'].values[0]
+    nearest_lat = station['latitude'].values[0]
     nearest_lon = station['longitude'].values[0]
-    ###
-
-    print(nearest_alt,nearest_lon,'versus: ',lat,lon)
 
     start = datetime(int(start_date[0:4]),
                     int(start_date[5:7]),
                     int(start_date[8:10]))
     end = datetime(2022, 12, 31)
     
-    #line_plot = plot_weather_data(lat, lon, start, end)
-    line_plot = plot_weather_data(nearest_alt, nearest_lon, start, end)
+    line_plot = plot_weather_data(nearest_lat, nearest_lon, start, end)
     
-    #box_plot = plot_tmax_boxplot(lat, lon, start, end, boxplot_variable)
-    box_plot = plot_tmax_boxplot(nearest_alt, nearest_lon, start, end, boxplot_variable)
+    box_plot = plot_tmax_boxplot(nearest_lat, nearest_lon, start, end, boxplot_variable)
     
     return line_plot, box_plot
     
